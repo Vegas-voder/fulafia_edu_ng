bc4badc7de36aa4a9985aa787327d369df827b0a
Eugene <orsareugene@gmail.com>
2026-09-25 00:47:09 +0100

    feat: add landing page hub + template stubs
    - index.html: speed-optimised landing hub with CDN fonts (Inter) + FA icons
    - Hero section with campus photo (assets/hero.jpg)
    - 5 template cards: Faculty, Department, Directorate, Unit, Centre
    - Build progress tracker panel
    - templates/: placeholder stubs for all 5 template pages
    - No CSS framework - vanilla CSS with CSS custom properties
    - Font Awesome loaded async (media=print trick) for zero render-blocking

---
+++ make_stubs.py
@@ -0,0 +1,11 @@
+﻿import os, sys
+base = r'C:\Users\user\Desktop\fulafia_edu_ng\templates'
+os.makedirs(base, exist_ok=True)
+pages = [('faculty','Faculty'),('department','Department'),('directorate','Directorate'),('unit','Unit'),('centre','Centre')]
+t = open(r'C:\Users\user\Desktop\fulafia_edu_ng\make_stubs.py').read()
+for slug, title in pages:
+    html = '<html><body><h1>' + title + '</h1><a href=../index.html>Back</a></body></html>'
+    with open(os.path.join(base, slug+'.html'),'w') as f:
+        f.write(html)
+    sys.stdout.write('OK: ' + slug + '\n')
+sys.stdout.write('DONE\n')
